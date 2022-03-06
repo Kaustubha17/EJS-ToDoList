188627d42a6005d3eaddf50a50d4fd4af140d05c
Kaustubha17 <kaustubhashah@gmail.com>
2022-03-06 20:46:35 +0530

    Add formatted date

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const app=express();
 app.listen(3000,()=>{console.log("app is listening at port 3000")});
 app.set('view engine','ejs');
 app.get("/",(req,res)=>{
-    
-    let today=new Date().getDay();
+
+   /*
+************************************
+   for understanding concepts 
+************************************
+   */
+
+    // let today=new Date().getDay();
     // if(today.getDay()===0||6){
     //     res.send("Yay!! it's the weekend!");
     // }
@@ -15,38 +21,48 @@ app.get("/",(req,res)=>{
     // res.send("Boo, I have to work");
     // }
 
-let day="";
-    if(today==6){
-day="Saturday";
+// let day="";
+//     if(today==6){
+// day="Saturday";
 
-    }
-    else if(today==5){
-        day="Friday";
+//     }
+//     else if(today==5){
+//         day="Friday";
 
-    }
-    else if(today==4){
-        day="Thursday";
+//     }
+//     else if(today==4){
+//         day="Thursday";
        
-    }
+//     }
 
-    else if(today==3){
-        day="Wednesday";
-    }
-     else if(today==2){
-        day="Tuesday";
+//     else if(today==3){
+//         day="Wednesday";
+//     }
+//      else if(today==2){
+//         day="Tuesday";
         
-    }
-     else if(today==1){
-        day="Monday";
+//     }
+//      else if(today==1){
+//         day="Monday";
       
-    }
-     else if(today==0){
-        day="Sunday";
+//     }
+//      else if(today==0){
+//         day="Sunday";
         
-    }
-    res.render("list",{kindOfDay:day})
+//     }
+//     res.render("list",{kindOfDay:day})
     //This step requires .ejs file to be present in views folder if not present then creste it
     //res.render(filename in views folder with ejs extension, js object key being the name in ejs file and value being the varaible name in js  script )
-})
+
 
 //  <%  %> also known as scriplet tags --used for any javascipt code embedded also <% helps in getting value from javascript code 
+
+// 
+
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var today  = new Date();
+var date=today.toLocaleDateString("en-US", options);
+console.log(typeof(date));
+res.render('list',{date:date});
+
+})
